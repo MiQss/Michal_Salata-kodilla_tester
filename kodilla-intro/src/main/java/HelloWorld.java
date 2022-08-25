e176462f991f346fcd6a66a20b5074fcda488c0b
Michał Salata <MiQs@poczta.onet.pl>
2022-08-25 18:51:29 +0200

    HelloWorld

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+public static void main(String[] args) {         // [2]
+    System.out.println("Hello World"); // [3]
+    System.out.println("Michał");//
+    System.out.println(35);//
+    System.out.println('S');//
+}
+}
