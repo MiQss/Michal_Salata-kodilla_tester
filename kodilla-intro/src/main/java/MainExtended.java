5df86a624e3133a2646e8f5e533c7c598ad41abc
Michał Salata <MiQs@poczta.onet.pl>
2022-09-02 19:07:41 +0200

    MainExtended

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MainExtended {
-}
+public class MainExtended {
+    public static void main(String[] args){
+        String myCar = "Skoda";
+        String carColour = "White";
+        int year = 2020;
+        double engine = 1.0;
+        char carClass = 'C';
+        boolean isGoodDriver = true;
+        System.out.println(myCar);
+        System.out.println(carColour);
+        System.out.println(year);
+        System.out.println(engine);
+        System.out.println(carClass);
+        System.out.println(isGoodDriver);
+    }
+        }
