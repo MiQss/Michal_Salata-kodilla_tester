1c1d38f55f30f57e29b5dee42f8e13fadecf29df
Michał Salata <MiQs@poczta.onet.pl>
2022-09-03 18:08:47 +0200

    LeapYear

---
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -0,0 +1,16 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        int year = 2001;
+
+        System.out.println(year);
+
+        if (year%400==0  ) {
+            System.out.println("Przestępny");
+        } else if (year%100== 0 ) {
+            System.out.println("Nie Przestępny");
+        } else if (year%4== 0) {
+            System.out.println("Przestępny");
+        } else {
+            System.out.println("Nie Przestępny");}
+}
+}
