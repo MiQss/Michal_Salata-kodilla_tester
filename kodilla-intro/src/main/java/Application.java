36825a94632710b227a6f93f6af19d4c01281a19
Michał Salata <MiQs@poczta.onet.pl>
2022-09-20 16:49:34 +0200

    Application

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,21 +1,22 @@
 public class Application {
-    static String name;
+    String name;
     double age;
     double height;
     public Application (String name, double age, double height){
-        Application.name = name;
+        this.name = name;
         this.age = age;
         this.height = height;
     }
-    public static void main(String[] args) {
-        Application adam = new Application("Adam",40.5,178);
-
-        if (name != null) {
-            if (adam.age > 30 && adam.height > 160) {
-                System.out.println("User is older than 30 and taller than 160cm");
-            } else {
-                System.out.println("User is 30 (or younger) or 160cm (or shorter)");
-            }
+    public void validate(){
+        if (this.age>30&& this.height>160){
+            System.out.println("User is older than 30 and taller than 160cm");
+        }else {
+            System.out.println("User is 30 (or younger) or 160cm (or shorter)");
         }
     }
-}
+    public static void main(String[] args) {
+        Application adam = new Application("Adam",40.5,178);
+        adam.validate();
+        }
+    }
+
