048b3f25e169e33e512c0b162c2a0a3d6309fdcb
Michał Salata <MiQs@poczta.onet.pl>
2022-10-13 17:43:39 +0200

    6.1 UserList

--- src/main/java/com/kodilla/stream/UsersManager.java
+++ src/main/java/com/kodilla/stream/UsersManager.java
@@ -1,16 +1,22 @@
 package com.kodilla.stream;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UsersManager {
     public static void main(String[] args) {
-        processUsersStream();
+        List<String> chemistGroupUsernames = filterChemistGroupUsernames();
+        System.out.println(chemistGroupUsernames);
     }
 
-    private static void processUsersStream() {
-        UsersRepository.getUsersList()
+    private static List<String> filterChemistGroupUsernames() {
+        List<String> usernames = UsersRepository.getUsersList()
                 .stream()
                 .filter(user -> user.getGroup().equals("Chemists"))
                 .map(UsersManager::getUserName)
-                .forEach(username -> System.out.println(username));
+                .collect(Collectors.toList());
+
+        return usernames;
     }
     public static String getUserName(User user) {
         return user.getUsername();
