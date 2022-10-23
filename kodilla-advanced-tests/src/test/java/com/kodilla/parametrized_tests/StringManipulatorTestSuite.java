46b9f1ef1ea4b539368487f5ec3e8ba3dcb34332
Michał Salata <MiQs@poczta.onet.pl>
2022-10-23 15:50:24 +0200

    7.2 CSV

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -23,7 +23,7 @@ public class StringManipulatorTestSuite {
         }
 
         @ParameterizedTest
-        @CsvSource(value = {"te,st:1", "..OtHEr :0", "E,V,e,n.t:3", "null :0", "A:0"}, delimiter = ':')
+        @CsvFileSource(resources = "/CountNumberOfCommas.csv", numLinesToSkip = 1, delimiter = ':')
         public void shouldCountNumberOfCommas(String input, int expected) {
                 assertEquals(expected, manipulator.countNumberOfCommas(input));
         }
