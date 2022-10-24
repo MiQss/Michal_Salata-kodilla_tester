4594ba1288279a6971b67130e21a5f18eb7b6389
Michał Salata <MiQs@poczta.onet.pl>
2022-10-24 15:26:32 +0200

    7.3 PersonTestSuiteBMI

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/PersonData.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/PersonData.java
@@ -9,11 +9,17 @@ public class PersonData {
 
     private static Stream<Arguments> provideDataForTestingBMI() {
         return Stream.of(
+                Arguments.of(new Person(2.07,55)),
+                Arguments.of(new Person(1.97,60)),
+                Arguments.of(new Person(1.97,65)),
                 Arguments.of(new Person(1.87,85)),
+                Arguments.of(new Person(1.77,90)),
                 Arguments.of(new Person(1.77,95)),
                 Arguments.of(new Person(1.67,105)),
-                Arguments.of(new Person(1.97,65)),
-                Arguments.of(new Person(2.07,55))
+                Arguments.of(new Person(1.67,115)),
+                Arguments.of(new Person(1.57,120)),
+                Arguments.of(new Person(1.57,130)),
+                Arguments.of(new Person(1.57,160))
         );
     }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/PersonTestSuite.java
@@ -4,7 +4,9 @@ import com.kodilla.parametrized_tests.homework.Person;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PersonTestSuite {
 
@@ -12,6 +14,20 @@ public class PersonTestSuite {
     @MethodSource (value = "com.kodilla.parametrized_tests.PersonData#provideDataForTestingBMI")
     public void shouldCalculateBMIWithPersonDataSource(Person person){
         System.out.println(person.getBMI());
-        assertNotNull(person.getBMI());
+        Stream<String> expected = Stream.of(
+                "Very severely underweight",
+                "Severely underweight",
+                "Underweight",
+                "Normal (healthy weight)",
+                "Overweight",
+                "Obese Class I (Moderately obese)",
+                "Obese Class II (Severely obese)",
+                "Obese Class III (Very severely obese)",
+                "Obese Class IV (Morbidly Obese)",
+                "Obese Class V (Super Obese)",
+                "Obese Class VI (Hyper Obese)"
+        );
+        boolean match = expected.anyMatch(un -> un.equals(person.getBMI()));
+        assertTrue(match);
     }
 }
