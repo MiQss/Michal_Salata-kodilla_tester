28f74b7c68805f376925a81c8184d2808c220cbb
Michał Salata <MiQs@poczta.onet.pl>
2022-10-25 11:10:24 +0200

    7.3 PersonTestSuiteBMI

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/PersonTestSuite.java
@@ -4,30 +4,14 @@ import com.kodilla.parametrized_tests.homework.Person;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PersonTestSuite {
 
     @ParameterizedTest
     @MethodSource (value = "com.kodilla.parametrized_tests.PersonData#provideDataForTestingBMI")
-    public void shouldCalculateBMIWithPersonDataSource(Person person){
-        System.out.println(person.getBMI());
-        Stream<String> expected = Stream.of(
-                "Very severely underweight",
-                "Severely underweight",
-                "Underweight",
-                "Normal (healthy weight)",
-                "Overweight",
-                "Obese Class I (Moderately obese)",
-                "Obese Class II (Severely obese)",
-                "Obese Class III (Very severely obese)",
-                "Obese Class IV (Morbidly Obese)",
-                "Obese Class V (Super Obese)",
-                "Obese Class VI (Hyper Obese)"
-        );
-        boolean match = expected.anyMatch(un -> un.equals(person.getBMI()));
-        assertTrue(match);
+    public void shouldCalculateBMIWithPersonDataSource(Person input, String expected){
+        assertEquals(expected, input.getBMI());
+
     }
 }
