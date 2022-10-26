5ae087ee10f0bb17f57360e20c32182d1d04794d
Michał Salata <MiQs@poczta.onet.pl>
2022-10-26 09:18:07 +0200

    7.3 PersonTestSuiteBMI

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/PersonData.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/PersonData.java
@@ -10,9 +10,16 @@ public class PersonData {
     private static Stream<Arguments> provideDataForTestingBMI() {
         return Stream.of(
                 Arguments.of(new Person(2.07,55),"Very severely underweight"),
+                Arguments.of(new Person(1.97,60),"Severely underweight"),
+                Arguments.of(new Person(1.97,65),"Underweight"),
                 Arguments.of(new Person(1.87,85),"Normal (healthy weight)"),
-                Arguments.of(new Person(1.57,130),"Obese Class V (Super Obese)")
-
+                Arguments.of(new Person(1.77,90),"Overweight"),
+                Arguments.of(new Person(1.77,95),"Obese Class I (Moderately obese)"),
+                Arguments.of(new Person(1.67,105),"Obese Class II (Severely obese)"),
+                Arguments.of(new Person(1.67,115),"Obese Class III (Very severely obese)"),
+                Arguments.of(new Person(1.57,120),"Obese Class IV (Morbidly Obese)"),
+                Arguments.of(new Person(1.57,130),"Obese Class V (Super Obese)"),
+                Arguments.of(new Person(1.57,160),"Obese Class VI (Hyper Obese)")
         );
     }
 }
