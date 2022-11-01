c3de9cf3518680a029f260e687425a9e9c9b8b5d
Michał Salata <MiQs@poczta.onet.pl>
2022-11-01 20:15:13 +0100

    9.1 DeliveryService

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -0,0 +1,10 @@
+package com.kodilla.spring.basic.dependency_injection.homework;
+
+public class Application {
+    public static void main(String[] args) {
+        DeliveryService deliveryService = new DeliveryService();
+        NotificationService notificationService = new NotificationService();
+    ShippingCenter shippingCenter = new ShippingCenter(notificationService, deliveryService);
+    shippingCenter.sendPackage("Hill Street 11, New York", 18.2);
+}
+}
