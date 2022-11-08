67dbc5cebe1d095575c610516059f4ad3fa8b282
Michał Salata <MiQs@poczta.onet.pl>
2022-11-08 15:27:47 +0100

    9.5 ClockTestSuite

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_scopes/homework/Clock.java
@@ -8,9 +8,15 @@ import java.time.LocalTime;
 @Component
 @Scope("prototype")
 public class Clock {
+    private LocalTime localTime;
 
     public Clock() {
         LocalTime localTime = LocalTime.now();
         System.out.println(localTime);
     }
+    public LocalTime getLocalTime(){
+        return localTime;
+    }
+
 }
+
