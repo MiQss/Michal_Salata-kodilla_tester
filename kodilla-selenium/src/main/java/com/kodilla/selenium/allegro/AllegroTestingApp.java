b9766fc4f19299d66f23c950bca4edc740a5bcef
Michał Salata <MiQs@poczta.onet.pl>
2022-12-02 19:07:29 +0100

    13.5 AllegroTestingApp CSS

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,7 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
 
@@ -12,21 +16,29 @@ public class AllegroTestingApp {
         System.setProperty("webdriver.gecko.driver", "c:\\selenium-drivers\\firefox\\geckodriver.exe");    // [1]
         WebDriver driver = new FirefoxDriver();
         driver.get("https://allegro.pl/");
+        WebDriverWait wait = new WebDriverWait(driver, 20);
 
-        WebElement cookies = driver.findElement(By.xpath("//*[@id=\"opbox-gdpr-consents-modal\"]/div/div[2]/div/div[2]/button[1]"));
+        WebElement cookies = driver.findElement(By.cssSelector("button[data-role*=accept-consent]"));
         cookies.click();
 
         Thread.sleep(1000);
 
-        WebElement searchField = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div/div/div/div/div[3]/header/div/div/div/div/form/input"));
+        WebElement searchField = driver.findElement(By.cssSelector("input.msts_pt"));
         searchField.sendKeys("Mavic mini");
 
         Thread.sleep(1000);
 
-        WebElement categoryCombo = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div/div/div/div/div[3]/header/div/div/div/div/form/div[3]/div/select"));
+        WebElement categoryCombo = driver.findElement(By.cssSelector("select.mr3m_1"));
         Select categorySelect = new Select(categoryCombo);
         categorySelect.selectByIndex(3);
         categoryCombo.submit();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section > article")));
+        List<WebElement> element = driver.findElements(By.cssSelector("section > article"));
+
+        System.out.println(element.get(3).getText());
+
+
 
     }
 }
