c0cb007d735c664f38c96e08c261d30cbbc6a945
Michał Salata <MiQs@poczta.onet.pl>
2022-12-11 13:11:59 +0100

    14.3 WalletSteps

--- kodilla-cucumber/src/test/java/TestRunner.java
+++ kodilla-cucumber/src/test/java/TestRunner.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/homework"}
+        features = {"src/test/resources"}
 )
 public class TestRunner {
 }
